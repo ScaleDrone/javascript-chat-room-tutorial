eb5ac559e04ef1f18617aaade5bac3958f4c10cd
herkyl <s.herkyl@gmail.com>
2016-12-23 19:50:48 +0200

    More cleanups

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const DOM = {
   membersList: document.querySelector('.members-list'),
   messages: document.querySelector('.messages'),
   input: document.querySelector('.message-form__input'),
+  form: document.querySelector('.message-form'),
 };
 
 const CLIENT_ID = 'G3TYvCzoXtrIuEtQ';
@@ -42,14 +43,18 @@ drone.on('open', error => {
   });
 
   room.on('member_leave', ({id}) => {
-    const index = members.findIndex(m => m.id === id);
+    const index = members.findIndex(member => member.id === id);
     members.splice(index, 1);
     updateMembersCountDOM();
     updateMembersListDOM();
   });
 
-  room.on('data', (text, {id}) => {
-    addMessageToListDOM(id, text);
+  room.on('data', (text, member) => {
+    if (member) {
+      addMessageToListDOM(text, member);
+    } else {
+      // Message is from server
+    }
   });
 });
 
@@ -64,30 +69,29 @@ drone.on('error', error => {
 function getRandomName() {
   const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
   const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-  const rnd = Math.floor(Math.random() * Math.pow(2, 12));
-  return adjs[rnd >> 6 % 64] + "_" + nouns[rnd % 64];
+  return (
+    adjs[Math.floor(Math.random() * adjs.length)] +
+    "_" +
+    nouns[Math.floor(Math.random() * nouns.length)]
+  );
 }
 
 function getRandomColor() {
-  return '#' + Math.floor(Math.random()*0xFFFFFF).toString(16);
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
+
+DOM.form.addEventListener('submit', sendMessage);
 
 function sendMessage() {
   const value = DOM.input.value;
   if (value === '') {
-    return false;
+    return;
   }
   DOM.input.value = '';
   drone.publish({
     room: 'observable-room',
     message: value,
   });
-  return false; // To prevent page refresh
-}
-sendMessage(); // to prevent ESLint error..
-
-function findMember(id) {
-  return members.find(m => m.id === id);
 }
 
 //------------- DOM STUFF
@@ -119,10 +123,10 @@ function createMessageElement(text) {
   return el;
 }
 
-function addMessageToListDOM(id, text) {
+function addMessageToListDOM(text, member) {
   const el = DOM.messages;
   const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
-  el.appendChild(createMemberElement(findMember(id)));
+  el.appendChild(createMemberElement(member));
   el.appendChild(createMessageElement(text));
   if (wasTop) {
     el.scrollTop = el.scrollHeight - el.clientHeight;
