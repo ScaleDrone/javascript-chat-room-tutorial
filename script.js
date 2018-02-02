b58cd24de2ba406d3a19fb6360fdbdf516198c5c
herkyl <s.herkyl@gmail.com>
2018-02-02 17:40:26 +0200

    Remove everything that is not needed for GitHub

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+// PS! Replace this with your own channel ID
+// If you use this channel ID your app will stop working in the future
 const CLIENT_ID = '4cNswoNqM2wVFHPg';
-//const CLIENT_ID = '2BQI1Uc8yaEAnupY';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
     name: getRandomName(),
     color: getRandomColor(),
   },
-  //url: 'ws://localhost:9000/websocket'
 });
 
 let members = [];
